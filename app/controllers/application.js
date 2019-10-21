10db9a82b992fd4684ec466a05240763aa3d9b29
Erika Pauwels <erika.pauwels@gmail.com>
2019-10-21 11:16:52 +0200

    Fix syntax error

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,7 +3,7 @@ import Controller from '@ember/controller';
 export default class ApplicationController extends Controller {
   constructor() {
     super(...arguments);
-    const isProducer = window.location.port == "81" || window.location.host = 'books.rpio-dev.s.redpencil.io';
-    this.role = isProducer ? "producer" : "consumer";
+    const isProducer = window.location.port == '81' || window.location.host == 'books.rpio-dev.s.redpencil.io';
+    this.role = isProducer ? 'producer' : 'consumer';
   }
 }
